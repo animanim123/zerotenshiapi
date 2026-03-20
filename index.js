21f30965fd471ab6f0a9e1ad136fc6c11b542e0e
BAGASHIDAYAT07 <niganiga0770@gmail.com>
2026-03-20 15:19:29 +0700

    benerin lagi vercel

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ app.get("/home", async (req, res) => {
     try {
         const response = await axios.get("https://mynimeku.com", {
             headers: {
-                "User-Agent": "Mozilla/5.0",
-                "Accept": "text/html",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Connection": "keep-alive",
+                "Upgrade-Insecure-Requests": "1"
             },
         });
 
@@ -135,8 +138,11 @@ app.get("/info/:katslug/*", async (req, res) => {
     try {
         const response = await axios.get(url, {
             headers: {
-                "User-Agent": "Mozilla/5.0",
-                "Accept": "text/html",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Connection": "keep-alive",
+                "Upgrade-Insecure-Requests": "1"
             },
         });
 
@@ -265,8 +271,11 @@ app.get("/content/:katcontent/*", async (req, res) => {
     try {
         const response = await axios.get(url, {
             headers: {
-                "User-Agent": "Mozilla/5.0",
-                "Accept": "text/html",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Connection": "keep-alive",
+                "Upgrade-Insecure-Requests": "1"
             },
         });
 
