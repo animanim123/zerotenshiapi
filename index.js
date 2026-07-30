713b9919e7a92ed2394a5dcac0bc603312fd9fbd
BANG GAZZ <niganiga0770@gmail.com>
2026-07-30 14:12:52 +0700

    benerin bagian content

--- index.js
+++ index.js
@@ -354,7 +354,7 @@ app.get("/content/:katcontent/*", async (req, res) => {
       const img = $(el).attr("src")?.replace(/^\/\//, "https://");
       const imgId = $(el).attr("img-id");
       komik.push({
-        img: `https://zerotenshiapi.vercel.app/image?url=${encodeURIComponent(img)}`,
+        img,
         imgId,
       });
     });
